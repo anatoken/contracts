b34feb9affafe7094eb9d0955b13967a280b600c
prusinskaite <prushinskaite@gmail.com>
2020-01-13 14:23:23 +0100

    Added recycle functionality

--- Recycle.sol
+++ Recycle.sol
@@ -1,10 +1,18 @@
-pragma solidity ^0.5.12;
+pragma solidity >=0.5.11;
 import "./access-control/RBAC.sol"; /// We should use this for role based access. as specified in our docs
+import "./Token.sol";
 
 contract Reycle{
 
     uint public averageTimeToCollectPlastic = 2;
     uint public totalCollectedPlastic = 0;
+    uint public minWeight = 0;
+    Token public anaToken;
+
+    // Constructor. Pass it the token you want this contract to work with
+    constructor(Token _token) public {
+        anaToken = _token;
+    }
 
     struct CollectedPlastic{
         address collector;
@@ -13,21 +21,90 @@ contract Reycle{
         uint dateCollected;
     }
 
-    /// create events and modifiers here
+    event PlasticIsAccounted(
+        address collector,
+        uint collectedPlasticId,
+        uint collectedPlasticKg,
+        uint collectedPlasticDate,
+        uint currentAvarageTimeToCollectPlastic
+    );
 
-    /// mappings
-    mapping(uint => address) public plasticToOwner;
-    mapping(address => uint) ownerPlasticCount;
+    event MinWeightIsUpdated(
+        uint oldMinWeight,
+        uint minWeight,
+        address byAddress
+    );
+
+    event AverageTimeToCollectPlasticIsUpdated(
+        uint oldAverageTimeToCollectPlastic,
+        uint newAverageTimeToCollectPlastic,
+        address byAddress
+    );
+
+    modifier validPlasticWeight(uint _kg) {
+        require(_kg > minWeight, "More plastic is required for minimum valid transaction.");
+        _;
+    }
+
+    mapping(uint => address) public plasticToCollector;
+    mapping(address => uint) collectorPlasticCount;
+    mapping(address => CollectedPlastic[]) collectorToCollectedPlastic;
 
     function _sendAnaTokenToCollector(address _collector, uint _kg) private {
-        ///based on the weight send anatoken to collector
+        uint256 id = getRandom();
+        uint dateCollected = now;
+        collectorToCollectedPlastic[_collector].push( CollectedPlastic(_collector, id, _kg, dateCollected));
+        plasticToCollector[id] = _collector;
+        collectorPlasticCount[_collector] += _kg;
+
+        // TODO
+        // D: Delegate, doing the transaction for A, and paying for the gas.
+        uint tokens = _convertKilogramsToAnatoken(_kg);
+        Token.delegatedTransfer(
+            Nonce,  // N: Nonce
+            address(this), // A: Sender of the payment
+            _collector, // B: Recipient of the payment
+            tokens, // X: Amount of Token T sent from A to B
+            Y, // Y: Fee paid in Token T, from A to D for the transaction
+            V,
+            R,
+            S
+        );
+        emit PlasticIsAccounted(
+            _collector,
+            id,
+            _kg,
+            dateCollected,
+            averageTimeToCollectPlastic
+        );
     }
 
     function sendAnaTokenToCollector(address collector, uint kg) public payable {
         _sendAnaTokenToCollector(collector, kg);
     }
 
-    function getCollectedPlasticFromUser(address user) external view returns (uint[] memory) {
+    function getCollectedPlasticFromUser(address user) external view returns (uint) {
         //get all the collected plastic from a user
+        return collectorPlasticCount[user];
+    }
+
+    function _convertKilogramsToAnatoken(uint _kg) private returns(uint anaTokensAmount) {
+        return _kg * averageTimeToCollectPlastic;
+    }
+
+    function updateAverageTimeToCollectPlastic(uint newValueAverageTimeToCollectPlastic) public{
+        uint oldAverageTimeToCollectPlastic = averageTimeToCollectPlastic;
+        averageTimeToCollectPlastic = newValueAverageTimeToCollectPlastic;
+        emit AverageTimeToCollectPlasticIsUpdated(oldAverageTimeToCollectPlastic, averageTimeToCollectPlastic, msg.sender);
+    }
+
+    function updateMinWeight(uint newMinWeight) public{
+        uint oldMinWeight = minWeight;
+        minWeight = newMinWeight;
+        emit MinWeightIsUpdated(oldMinWeight, minWeight, msg.sender);
+    }
+
+     function getRandom() private view returns(uint256) {
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.coinbase, block.timestamp))); //TODO improve
     }
 }
