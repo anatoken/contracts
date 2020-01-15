67d5795a4640cd07f9b84c1aef89d63c4718ca94
djamaile <djamaile.rahamat@minbzk.nl>
2020-01-15 13:55:49 +0100

    created roles

--- Recycle.sol
+++ Recycle.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.5.12;
-import "./access-control/RBAC.sol"; /// We should use this for role based access. as specified in our docs
+import "./access-control/RBACExtend"; /// We should use this for role based access. as specified in our docs
 
 contract Reycle{
-
+    
+  
     uint public averageTimeToCollectPlastic = 2;
     uint public totalCollectedPlastic = 0;
+    bool public rightRole;
+    RBACExtend roleHelper;
 
     struct CollectedPlastic{
         address collector;
@@ -12,14 +15,32 @@ contract Reycle{
         uint kg;
         uint dateCollected;
     }
+    
+    constructor() public {
+        roleHelper = new RBACExtend();
+    }
 
     /// create events and modifiers here
+    modifier hasCollectorRole { require(roleHelper.userHasRole("collector"), "User doesn't have this role"); _;}
+    modifier hasRecyclePlantRole { require(roleHelper.userHasRole("recyclePlant"), "User doesn't have this role"); _;}
+    modifier hasRootRole {require(roleHelper.userHasRole("ROOT"), "User doesn't have this role"); _;} //might not be needed
 
     /// mappings
     mapping(uint => address) public plasticToOwner;
     mapping(address => uint) ownerPlasticCount;
-
-    function _sendAnaTokenToCollector(address _collector, uint _kg) private {
+    
+    /*
+     * Example function on how to use the RBACExtend contract
+    */
+    function roleExampleFunction() public hasRootRole view returns(string memory){
+        return "Has role";
+    }
+    
+    function _roleExampleFunction() public payable  returns (string memory) {
+        roleExampleFunction();
+    }
+    
+    function _sendAnaTokenToCollector(address _collector, uint _kg) private hasRecyclePlantRole {
         ///based on the weight send anatoken to collector
     }
 
@@ -27,7 +48,8 @@ contract Reycle{
         _sendAnaTokenToCollector(collector, kg);
     }
 
-    function getCollectedPlasticFromUser(address user) external view returns (uint[] memory) {
+    function getCollectedPlasticFromUser(address user) external hasCollectorRole view returns (uint[] memory) {
         //get all the collected plastic from a user
     }
 }
+
--- access-control/RBACExtend.sol
+++ access-control/RBACExtend.sol
@@ -7,20 +7,30 @@ import "./RBAC.sol";
 contract RBACExtend {
     RBAC internal roles;
     
-    constructor(address _root) public{
-        roles = new RBAC(_root);
+    constructor() public{
+        roles = new RBAC(msg.sender);
         roles.addRole(stringToBytes32("collector"), stringToBytes32("ROOT"));
         roles.addRole(stringToBytes32("university"), stringToBytes32("ROOT"));
         roles.addRole(stringToBytes32("recyclePlant"), stringToBytes32("ROOT"));
     }
     
     
+    
     /**
      * @notice A method that checks if the contract has this role. This is mostly for debugging reasons. 
      * @param roleId The role that we want to check if it exists
      */
-    function contractHasRole(string memory roleId) public view returns (bool){
-        return roles.roleExists(stringToBytes32(roleId));
+    function userHasRole(string memory roleId) public view returns (bool){
+        return roles.hasRole(msg.sender, stringToBytes32(roleId));
+    }
+    
+    /**
+     * @notice A method that adds users to a specific role. This function is mostly for the front-end to register universities & plastic recycle plants.  
+     * @param user address of the user 
+     * @param roleId role to which user going to be added to 
+     */
+    function addUserToRole(address user, string memory roleId) private {
+        roles.addMember(user, stringToBytes32(roleId));
     }
     
     
